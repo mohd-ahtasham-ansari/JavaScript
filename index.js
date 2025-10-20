97df30048744256f27327283b7e56b8385db0a56
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-10-20 15:52:12 +0530

    operators in js

--- index.js
+++ index.js
@@ -6,18 +6,18 @@
 
 var number= 10;
 var number2 = 29.78;
-console.log(number + number2);
+// console.log(number + number2);
 
 //
 //STRINGS
 var string1 ="hello world";
 var string2 ='this is a javascript string variale';
-console.log(string1 + " ! " + string2);
+// console.log(string1 + " ! " + string2);
 
 //BOOLEAN
 var bool1 = true;
 var bool2 = false;
-console.log(bool1 + " " + bool2);
+// console.log(bool1 + " " + bool2);
 
 //OBJECTS
 var marks={
@@ -26,16 +26,29 @@ var marks={
     sam:100,
     shahzi:99.999
 }
-console.log(marks)
+// console.log(marks)
 
 //UNDEFINED AND NULL
 var und;
-console.log(und);
+// console.log(und);
 
 var n=null;
-console.log(n);
+// console.log(n);
 
 //ARRAYS
 var arr=[1,2,3,4,4,5.,'h',6 , true]
-console.log(arr);
-console.log(arr[3]);
+// console.log(arr);
+// console.log(arr[3]);
+
+//symbol    
+var sym1= Symbol("This is symbol 1");
+var sym2= Symbol("This is symbol 2");
+// console.log(sym1 === sym2); // false because symbols are unique 
+
+/*
+At very high level, there are two types of data types in javascript
+1. Primitive data types: undefined, null, number, string, boolean, symbol
+2. Reference data types: Arrays and Objects
+*/
+
+
---
+++ index2.js
@@ -0,0 +1,22 @@
+// OPERATORS IN JAVASCRIPT
+
+
+// Arithmetic Operators
+var a=110;
+var b=10;
+console.log("The value of a + b is " + (a + b));
+console.log("The value of a - b is " + (a - b));
+console.log("The value of a * b is " + (a * b));
+console.log("The value of a / b is " + (a / b));    
+
+// Assignment Operators
+var c=b;
+console.log("The value of c is " + c);
+c += 2; // c = c + 2
+console.log("The value of c after c += 2 is " + c);
+c -= 2; // c = c - 2
+console.log("The value of c after c -= 2 is " + c);
+c *= 2; // c = c * 2
+console.log("The value of c after c *= 2 is " + c);
+c /= 2; // c = c / 2
+console.log("The value of c after c /= 2 is " + c); 
