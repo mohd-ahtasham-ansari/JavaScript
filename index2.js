9a4b66b25bf9ac06f411d977f8f7f7f157bf5e4e
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-10-20 16:06:46 +0530

    operators in javascript

--- index2.js
+++ index2.js
@@ -20,3 +20,30 @@ c *= 2; // c = c * 2
 console.log("The value of c after c *= 2 is " + c);
 c /= 2; // c = c / 2
 console.log("The value of c after c /= 2 is " + c); 
+
+//COMPARISON OPERATORS
+var x=34;
+var y = 56;
+console.log(x == y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(x > y);
+console.log(x < y);
+console.log(x != y);
+
+console.log("logical operators");
+//LOGICAL OPERATORS
+
+//logical AND
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+
+//logical OR
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);        
+ 
+//logical NOT
+console.log(!true);
+console.log(!false);
