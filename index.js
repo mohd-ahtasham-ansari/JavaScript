2ba6231e5d5d64d4015898c1653d392f767e1125
mohd ahtasham ansari <aahtasham102@gmail.com>
2025-10-20 11:29:53 +0530

    javascript basics

---
+++ index.js
@@ -0,0 +1,41 @@
+// alert("hello world");
+// console.log("hello world");
+
+
+//NUMBERS
+
+var number= 10;
+var number2 = 29.78;
+console.log(number + number2);
+
+//
+//STRINGS
+var string1 ="hello world";
+var string2 ='this is a javascript string variale';
+console.log(string1 + " ! " + string2);
+
+//BOOLEAN
+var bool1 = true;
+var bool2 = false;
+console.log(bool1 + " " + bool2);
+
+//OBJECTS
+var marks={
+    ehte:99,
+    rafi:98,
+    sam:100,
+    shahzi:99.999
+}
+console.log(marks)
+
+//UNDEFINED AND NULL
+var und;
+console.log(und);
+
+var n=null;
+console.log(n);
+
+//ARRAYS
+var arr=[1,2,3,4,4,5.,'h',6 , true]
+console.log(arr);
+console.log(arr[3]);
